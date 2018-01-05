bc870d88a0aebefae259275c7ccf6ff9edc96db6
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-01-05 09:55:27 +0530

    err: no image

---
+++ checkBal.py
@@ -0,0 +1,3 @@
+from bit import Key
+my_key = Key('3G8XYSJCBCzWcdcrWQW1e4Nm9CLbt9gq3X')
+my_key.get_balance('usd')
---
+++ genKey.py
@@ -0,0 +1,4 @@
+# Status: Throws error; Image not found
+
+from bit import Key, PrivateKey, PrivateKeyTestnet
+Key == PrivateKey
